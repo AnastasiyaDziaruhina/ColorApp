ee1ab6aeb3400614be18da6b0a407d1e4b95f488
AnastasiyaDziaruhina <stacy.deryugina@gmail.com>
2023-05-08 20:16:32 +0200

    Refactored

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateLabels()
     }
 
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateColor(alpha: sender.value)
+        updateLabels()
+    }
 
+    
+    private func updateColor(alpha: Float) {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: CGFloat(alpha))
+        colorView.backgroundColor = color
+    }
+    
+    private func updateLabels() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
